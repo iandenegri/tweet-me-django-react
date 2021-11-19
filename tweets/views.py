aaaec16fc2d54b526b4d75090e06afe66952ab3e
Ian Denegri <iandenegri@hotmail.com>
2021-11-19 18:53:57 -0500

    I forgot to include this

--- tweets/views.py
+++ tweets/views.py
@@ -10,6 +10,8 @@ from rest_framework.permissions import IsAuthenticated
 # Local
 from .serializers import TweetSerializer, TweetActionSerializer, TweetCreateSerializer
 from .models import Tweet
+
+
 # Create your views here.
 
 def home(request, *args, **kwargs):
@@ -21,7 +23,34 @@ def home(request, *args, **kwargs):
         context=context,
         status=200)
 
+def tweet_list_view(request, *args, **kwargs):
+    context = {}
+    return render(
+        request, 
+        template_name="tweets/list.html",
+        context=context,
+        status=200)
 
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    context = {}
+    context['tweet_id'] =tweet_id
+    return render(
+        request, 
+        template_name="tweets/detail.html",
+        context=context,
+        status=200)
+
+def tweet_profile_view(request, username, *args, **kwargs):
+    can_tweet = 'true' if request.user == username else 'false'
+    context = {}
+    context['username'] = username
+    context['user_can_tweet'] = can_tweet
+    return render(
+        request, 
+        template_name="tweets/profile.html",
+        context=context,
+        status=200)
+# API
 @api_view(['GET'])
 def tweet_list(request, *args, **kwargs):
     qs = Tweet.objects.all()
